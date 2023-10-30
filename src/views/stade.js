48bb0d303688095f7795d49356268f850ace3852
hibafelfel <93076043+hibafelfel@users.noreply.github.com>
2023-10-30 23:49:13 +0100

    relations

--- src/views/stade.js
+++ src/views/stade.js
@@ -19,13 +19,13 @@ import React, { useEffect, useState } from "react";
 
 // reactstrap components
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Table,
-  Row,
-  Col,
+    Card,
+    CardBody,
+    CardHeader,
+    CardTitle,
+    Table,
+    Row,
+    Col,
 } from "reactstrap";
 
 // core components
@@ -34,91 +34,157 @@ import PanelHeader from "components/PanelHeader/PanelHeader.js";
 import { thead, tbody } from "variables/general";
 import axios from "axios";
 
+const stade = () => {
+    const [tkeys, setKeys] = useState([]);
+    const [tContent, setContent] = useState([]);
+    const [tkeysR, setKeysR] = useState([]);
+    const [tContentR, setContentR] = useState([]);
 
-const stade=()=> {
-  const [tkeys, setkeys] = useState([]);
-  const [tContent, setcontent] = useState([{
-    className: "",
-    data: [],
-  }]);
+   
   useEffect(() => {
-    axios.get(`http://localhost:8099/stade`)
-      .then(res => {
+    // Fetch data for the "trophee" table
+    axios.get("http://localhost:8099/stade")
+      .then((res) => {
         const value = res.data.results.bindings;
 
-        // Extract the keys from the first result
         if (value.length > 0) {
-          setkeys(Object.keys(value[0]).slice(1));
+          setKeys(Object.keys(value[0]).slice(1));
         }
-        value.forEach((val) => {
-      
+
+        const content = value.map((val) => {
           const obj = {
             className: "",
-            data: Object.values(val).map((property) => property.value).slice(1),
-         
+            data: Object.values(val).map((property) => {
+              return property.value.replace("http://www.semanticweb.org/lenovo/ontologies/2023/9/untitled-ontology-2#", "");
+            }).slice(1),
           };
-          
-          setcontent((prevContent) => [...prevContent, obj]);
+          return obj;
         });
 
-       
+        setContent(content);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  }, []); // The empty array means this effect runs once after the initial render
-  useEffect(() => {console.log(tContent)},[tContent])
-  return (
-    <>
-      <PanelHeader size="sm" />
-      <div className="content">
-        <Row>
-          <Col xs={12}>
-            <Card>
-              <CardHeader>
-                <CardTitle tag="h4">stade</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead className="text-primary">
-                    <tr>
-                      {tkeys.map((prop, key) => {
-                        if (key === thead.length - 1)
-                          return (
-                            <th key={key} className="text-right">
-                              {prop}
-                            </th>
-                          );
-                        return <th key={key}>{prop}</th>;
-                      })}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tContent.map((prop, key) => {
-                      return (
-                        <tr key={key}>
-                          {prop.data.map((prop, key) => {
-                            if (key === thead.length - 1)
-                              return (
-                                <td key={key} className="text-right">
-                                  {prop}
-                                </td>
-                              );
-                            return <td key={key}>{prop}</td>;
-                          })}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        
-        </Row>
-      </div>
-    </>
-  );
+
+        // Fetch data for the "relationteamtrophee" table
+        axios.get("http://localhost:8099/relationstadevillematch")
+      .then((res) => {
+        const value = res.data.results.bindings;
+
+        if (value.length > 0) {
+          setKeysR(Object.keys(value[0]));
+        }
+
+        const content = value.map((val) => {
+          const obj = {
+            className: "",
+            data: Object.values(val).map((property) => {
+              return property.value.replace("http://www.semanticweb.org/lenovo/ontologies/2023/9/untitled-ontology-2#", "");
+            }),
+          };
+          return obj;
+        });
+
+        setContentR(content);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+    return (
+        <>
+            <PanelHeader size="sm" />
+            <div className="content">
+                <Row>
+                    <Col xs={12}>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle tag="h4">stade</CardTitle>
+                            </CardHeader>
+                            <CardBody>
+                                <Table responsive>
+                                    <thead className="text-primary">
+                                        <tr>
+                                            {tkeys.map((prop, key) => {
+                                                if (key === thead.length - 1)
+                                                    return (
+                                                        <th key={key} className="text-right">
+                                                            {prop}
+                                                        </th>
+                                                    );
+                                                return <th key={key}>{prop}</th>;
+                                            })}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {tContent.map((prop, key) => {
+                                            return (
+                                                <tr key={key}>
+                                                    {prop.data.map((prop, key) => {
+                                                        if (key === thead.length - 1)
+                                                            return (
+                                                                <td key={key} className="text-right">
+                                                                    {prop}
+                                                                </td>
+                                                            );
+                                                        return <td key={key}>{prop}</td>;
+                                                    })}
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </Table>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col xs={12}>
+                        <Card className="card-plain">
+                            <CardHeader>
+                                <CardTitle tag="h4">relation stade ville match</CardTitle>
+                                <p className="category"> Here is a subtitle for this table</p>
+                            </CardHeader>
+                            <CardBody>
+                                <Table responsive>
+                                    <thead className="text-primary">
+                                        <tr>
+                                            {tkeysR.map((prop, key) => {
+                                                if (key === thead.length - 1)
+                                                    return (
+                                                        <th key={key} className="text-right">
+                                                            {prop}
+                                                        </th>
+                                                    );
+                                                return <th key={key}>{prop}</th>;
+                                            })}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {tContentR.map((prop, key) => {
+                                            return (
+                                                <tr key={key}>
+                                                    {prop.data.map((prop, key) => {
+                                                        if (key === thead.length - 1)
+                                                            return (
+                                                                <td key={key} className="text-right">
+                                                                    {prop}
+                                                                </td>
+                                                            );
+                                                        return <td key={key}>{prop}</td>;
+                                                    })}
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </Table>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        </>
+    );
 }
 
 export default stade;
